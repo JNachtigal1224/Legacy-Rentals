3cf2421d975d1c73158daf31846c6d0e3b07724b
dasingleton <44787904+dasingleton@users.noreply.github.com>
2018-11-26 16:45:08 -0500

    test

--- Code/Web Application Prototype/WindowsFormsApp1/WindowsFormsApp1/Home.Designer.cs
+++ Code/Web Application Prototype/WindowsFormsApp1/WindowsFormsApp1/Home.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.manageButton = new System.Windows.Forms.Button();
@@ -38,85 +39,58 @@
             // 
             // panel1
             // 
-            this.panel1.AutoSize = true;
-            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.manageButton);
             this.panel1.Controls.Add(this.homeButton);
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
-            this.panel1.MaximumSize = new System.Drawing.Size(1920, 0);
+            resources.ApplyResources(this.panel1, "panel1");
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1870, 119);
-            this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Font = new System.Drawing.Font("Arial Black", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.button1, "button1");
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(1619, 25);
-            this.button1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(243, 69);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "SIGN IN";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click_3);
             // 
             // manageButton
             // 
-            this.manageButton.Location = new System.Drawing.Point(293, 7);
-            this.manageButton.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            resources.ApplyResources(this.manageButton, "manageButton");
             this.manageButton.Name = "manageButton";
-            this.manageButton.Size = new System.Drawing.Size(248, 105);
-            this.manageButton.TabIndex = 2;
-            this.manageButton.Text = "Manage";
             this.manageButton.UseVisualStyleBackColor = true;
             this.manageButton.Click += new System.EventHandler(this.button1_Click_2);
             // 
             // homeButton
             // 
-            this.homeButton.Location = new System.Drawing.Point(32, 7);
-            this.homeButton.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            resources.ApplyResources(this.homeButton, "homeButton");
             this.homeButton.Name = "homeButton";
-            this.homeButton.Size = new System.Drawing.Size(245, 105);
-            this.homeButton.TabIndex = 0;
-            this.homeButton.Text = "Home";
             this.homeButton.UseVisualStyleBackColor = true;
             this.homeButton.Click += new System.EventHandler(this.button1_Click);
             // 
             // searchButton
             // 
-            this.searchButton.Location = new System.Drawing.Point(741, 190);
-            this.searchButton.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            resources.ApplyResources(this.searchButton, "searchButton");
             this.searchButton.Name = "searchButton";
-            this.searchButton.Size = new System.Drawing.Size(491, 93);
-            this.searchButton.TabIndex = 1;
-            this.searchButton.Text = "Search";
             this.searchButton.UseVisualStyleBackColor = true;
             this.searchButton.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Home
             // 
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
-            this.AutoSize = true;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.ClientSize = new System.Drawing.Size(1888, 992);
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.Controls.Add(this.searchButton);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.ForeColor = System.Drawing.Color.Black;
             this.Name = "Home";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Home";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
