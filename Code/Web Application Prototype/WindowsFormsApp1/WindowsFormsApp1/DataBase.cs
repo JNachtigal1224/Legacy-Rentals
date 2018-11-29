edcc7d2cea140cb39d35ddaba546065daf221815
dasingleton <44787904+dasingleton@users.noreply.github.com>
2018-11-29 12:29:39 -0500

    database

--- Code/Web Application Prototype/WindowsFormsApp1/WindowsFormsApp1/DataBase.cs
+++ Code/Web Application Prototype/WindowsFormsApp1/WindowsFormsApp1/DataBase.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
-using Google.Apis.Auth.OAuth2;
+﻿using Google.Apis.Auth.OAuth2;
 using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 
@@ -13,75 +12,96 @@ namespace WindowsFormsApp1
 {
     class DataBase
     {
-
-        static string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static string[] Scopes = {SheetsService.Scope.Spreadsheets};
         static string ApplicationName = "Legacy Rentals DataBase";
 
-        static void Main()
-        {
-            UserCredential credential;
+        String spreadsheetId = "1YPcGEQC6DMhw6WX2Q13gLFpgPBOgpozA-ywmeZyHONk";
 
-            using (var stream =
-                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+        static UserCredential credential;
+        static SheetsService service;
+
+        public DataBase()
+        {
+            using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
             {
-                // The file token.json stores the user's access and refresh tokens, and is created
-                // automatically when the authorization flow completes for the first time.
                 string credPath = "token.json";
                 credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
                     GoogleClientSecrets.Load(stream).Secrets,
                     Scopes,
                     "user",
                     CancellationToken.None,
-                    new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
+                    new FileDataStore(credPath, true)
+                ).Result;
             }
 
-            // Create Google Sheets API service.
-            var service = new SheetsService(new BaseClientService.Initializer()
+            service = new SheetsService(new BaseClientService.Initializer()
             {
                 HttpClientInitializer = credential,
                 ApplicationName = ApplicationName,
             });
-
-            // Define request parameters.
-            String spreadsheetId = "1YPcGEQC6DMhw6WX2Q13gLFpgPBOgpozA-ywmeZyHONk";
-
-            void writeData(String range, List<object> data)
-            {
-                var valueRange = new ValueRange();
-
-                valueRange.Values = new List<IList<object>> { data };
-
-                var appendRequest = service.Spreadsheets.Values.Append(valueRange, spreadsheetId, range);
-                appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
-                var appendReponse = appendRequest.Execute();
-            }
-
-            void readData(String range)
-            {
-                var valueRange = new ValueRange();
-
-                var oblist = new List<object>() { "Hello!", "This", "was", "insertd", "via", "C#" };
-                valueRange.Values = new List<IList<object>> { oblist };
-
-                var getRequest = service.Spreadsheets.Values.Get(spreadsheetId, range);
-                var getReponse = getRequest.Execute();
-
-                IList<IList<Object>> values = getReponse.Values;
-                if (values != null && values.Count > 0)
-                {
-                    Console.WriteLine("userId,username");
-                    foreach (var row in values)
-                    {
-                        Console.WriteLine("{0}, {1}", row[0], row[0]);
-                    }
-                }
-                Console.Read();
-            }
-
-            //readData("users!A2:A");
-            //writeData("users!A2:A", new List<object>() { "984957", "nancy_drew" });
         }
 
+        //API Documentation
+        //Method: readData(String range)
+        //String range: sheetName!Row:Column
+        //RequestType: Reads all data provided in range starting at Row and going through the entire column
+        public void writeData(String range, List<object> data)
+        {
+            var valueRange = new ValueRange();
+
+            valueRange.Values = new List<IList<object>> { data };
+
+            var appendRequest = service.Spreadsheets.Values.Append(valueRange, spreadsheetId, range);
+            appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+            var appendReponse = appendRequest.Execute();
+        }
+
+        //API Documentation
+        //Method: readData(String range)
+        //String range: sheetName!Row:Column
+        //RequestType: Reads all data provided in range starting at Row and going through the entire column
+        public void updateData(String range, List<object> data)
+        {
+            var valueRange = new ValueRange();
+
+            valueRange.Values = new List<IList<object>> { data };
+
+            var updateRequest = service.Spreadsheets.Values.Update(valueRange, spreadsheetId, range);
+            updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED;
+            var appendReponse = updateRequest.Execute();
+        }
+
+        //API Documentation
+        //Method: deleteData(String range)
+        //String range: sheetName!Row:Column
+        //RequestType: Deletes data at range
+        public void deleteData(String range)
+        {
+            var requestBody = new ClearValuesRequest();
+
+            var deleteRequest = service.Spreadsheets.Values.Clear(requestBody, spreadsheetId, range);
+            var deleteReponse = deleteRequest.Execute();
+        }
+
+        //API Documentation
+        //Method: readData(String range)
+        //String range: sheetName!Row:Column
+        //RequestType: Reads all data in range starting at Row and going through the entire column
+        public IList<IList<Object>> readData(String range)
+        {
+            var valueRange = new ValueRange();
+
+            var oblist = new List<object>() { "Hello!", "This", "was", "insertd", "via", "C#" };
+            valueRange.Values = new List<IList<object>> { oblist };
+
+            var getRequest = service.Spreadsheets.Values.Get(spreadsheetId, range);
+            var getReponse = getRequest.Execute();
+
+            if(getReponse.Values == null)
+            {
+                return null;
+            }
+            return getReponse.Values;
+        }
     }
 }
--- Code/Web Application Prototype/WindowsFormsApp1/WindowsFormsApp1/Program.cs
+++ Code/Web Application Prototype/WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace WindowsFormsApp1
 {
     static class Program
     {
 
-        [STAThread]
+        static DataBase database = new DataBase();
+
         static void Main()
         {
 
+            IList<IList<Object>> values = database.readData("users!A2:A");
 
-            
+            database.deleteData("users!A2");
+
+            database.writeData("users!A2:A", new List<object>() { "984957", "nancy_drew" });
+
+            //if (values != null && values.Count > 0)
+            //{
+            //    Console.WriteLine("userId,username");
+            //    foreach (var row in values)
+            //    {
+            //        Console.WriteLine("{0}, {1}", row[0], row[0]);
+            //    }
+            //}
+            //Console.Read();
+
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
