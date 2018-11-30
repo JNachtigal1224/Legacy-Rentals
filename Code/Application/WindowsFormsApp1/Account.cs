9bd66894945b187de6e69e1093951a34117246a5
dasingleton <44787904+dasingleton@users.noreply.github.com>
2018-11-30 14:12:44 -0500

    fix

---
+++ Code/Application/WindowsFormsApp1/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Account
+{
+    private static DataBase database = Program.database;
+
+    public static List<String> fetchAccount(String username)
+    {
+        int rowNumber = 2;
+        foreach (var row in database.readData("users!A2:F"))
+        {
+            if (row[0].Equals(username))
+            {
+                return new List<String> { rowNumber.ToString(), row[0].ToString(), row[1].ToString(), row[2].ToString(), row[4].ToString(), row[5].ToString() };
+            }
+            rowNumber++;
+        }
+        return null;
+    }
+
+    public static void printAccount(String username)
+    {
+        var account = Account.fetchAccount("dasingleton");
+        Console.WriteLine("username: " + account[1]);
+        Console.WriteLine("first name: " + account[2]);
+        Console.WriteLine("last name: " + account[3]);
+        Console.Read();
+    }
+
+    public static void createAccount(String username, String firstName, String lastName, String password)
+    {
+        if(Account.fetchAccount(username) == null)
+        {
+            database.writeData("users!A2", new List<object>() { username, firstName, lastName, password });
+        }
+    }
+     
+    public static void updateBilling(String username, String cardNumber, String securityCode)
+    {
+        String range = "users!" + "E" + Account.fetchAccount(username)[0] + ":F";
+        database.updateData(range, new List<object>() { cardNumber, securityCode });
+    }
+
+    public static void updatePassword(String username, String password)
+    {
+        String range = "users!" + "D" + Account.fetchAccount(username)[0];
+        database.updateData(range, new List<object>() { password });
+    }
+
+}
---
+++ Code/Application/WindowsFormsApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+class Program
+{
+
+    public static DataBase database = new DataBase();
+
+    static void Main()
+    {
+        //IList<IList<Object>> values = database.readData("users!A2:A");
+
+        //database.deleteData("users!A2");
+
+        Account.createAccount("dasingleton", "David", "Singleton", "abc123");
+
+        Account.updateBilling("dasingleton", "1234567898765432", "456");
+
+        Account.updatePassword("dasingleton", "123xyz");
+
+        Account.printAccount("dasingleton");
+
+        //if (values != null && values.Count > 0)
+        //{
+        //    Console.WriteLine("userId,username");
+        //    foreach (var row in values)
+        //    {
+        //        Console.WriteLine("{0}, {1}", row[0], row[0]);
+        //    }
+        //}
+        //Console.Read();
+
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new WindowsFormsApp1.Home());
+
+    }
+}
