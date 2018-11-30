0740ca436a3fb57315707ca0a9a8904c8f9680f7
dasingleton <44787904+dasingleton@users.noreply.github.com>
2018-11-29 19:14:01 -0500

    new

---
+++ Code/Application/Legacy Rentals/WindowsFormsApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+class Program
+{
+
+    static DataBase database = new DataBase();
+
+    static void Main()
+    {
+        IList<IList<Object>> values = database.readData("users!A2:A");
+
+        database.deleteData("users!A2");
+
+        database.updateData("users!A2",new List<object>(){"911"});
+
+        database.writeData("users!A2:A",new List<object>(){"984957","nancy_drew"});
+
+        //if (values != null && values.Count > 0)
+        //{
+        //    Console.WriteLine("userId,username");
+        //    foreach (var row in values)
+        //    {
+        //        Console.WriteLine("{0}, {1}", row[0], row[0]);
+        //    }
+        //}
+        //Console.Read();
+
+
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new WindowsFormsApp1.Home());
+
+    }
+}
