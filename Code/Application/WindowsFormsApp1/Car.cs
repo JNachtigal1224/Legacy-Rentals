ca3860956d442038fbdfde5021c3cd7e949e7daa
dasingleton <44787904+dasingleton@users.noreply.github.com>
2018-12-02 15:54:07 -0500

    cars

--- Code/Application/WindowsFormsApp1/Car.cs
+++ Code/Application/WindowsFormsApp1/Car.cs
@@ -19,19 +19,51 @@ class Car
         return null;
     }
 
-    public static void addCar(String carId)
+    public static List<List<String>> getCars(String loc, String mod, int seating, int maxPrice)
+    {
+        List<List<String>> cars = new List<List<String>> { };
+        foreach (var row in database.readData("cars!A2:F"))
+        {
+            if (row[2].Equals(loc))
+            {
+                if (mod.Equals("All Models") || row[1].Equals(mod))
+                {
+                    if (int.Parse(row[3].ToString()) <= maxPrice)
+                    {
+                        if (int.Parse(row[4].ToString()) >= seating)
+                        {
+                            if (row[5].Equals("true"))
+                            {
+                                List<String> info = new List<String> { row[0].ToString(), row[1].ToString(), row[2].ToString(),
+                                    row[3].ToString(), row[4].ToString(), row[5].ToString(), row[6].ToString()
+                                };
+                                Console.WriteLine(row[6].ToString());
+                                Console.Read();
+                                cars.Add(info);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return cars;
+    }
+
+    public static void addCar(String carId, String model, String loc, String price, String seating, String avail, String name)
     {
         if (Car.fetchCar(carId) == null)
         {
-            database.writeData("cars!A2", new List<object>() { carId });
+            database.writeData("cars!A2", new List<object>() { carId, model, loc, price, seating, avail, name });
         }
     }
 
     public static void removeCar(String carId)
     {
-        if (Car.fetchCar(carId) == null)
+        var data = Account.fetchAccount(carId);
+        if (data != null)
         {
-            database.updateData("cars!A2", new List<object>() { carId });
+            String range = "cars!" + "A" + data[0] + ":G";
+            database.deleteData(range);
         }
     }
 
