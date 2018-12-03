eba467c38318156e44c28a9e81f23c7b438e1c5d
dasingleton <44787904+dasingleton@users.noreply.github.com>
2018-12-02 20:16:53 -0500

    manage

--- Code/Application/WindowsFormsApp1/Manage.cs
+++ Code/Application/WindowsFormsApp1/Manage.cs
@@ -15,9 +15,37 @@ class Manage
         Car.removeCar(carId);
     }
 
-    public static void request(String requestId, String carId, String username)
+    public static void addRequest(String requestId, String carId, String username)
     {
         database.writeData("request!A2", new List<object>() { requestId, carId, username });
+    }
+
+    public static List<String> fetchRequest(String requestId)
+    {
+        var data = database.readData("request!A2:C");
+        if (data != null)
+        {
+            int rowNumber = 2;
+            foreach (var row in data)
+            {
+                if (row[0].Equals(requestId))
+                {
+                    return new List<String> { rowNumber.ToString(), row[0].ToString(), row[1].ToString(), row[2].ToString() };
+                }
+                rowNumber++;
+            }
+        }
+        return null;
+    }
+
+    public static void removeRequest(String requestId)
+    {
+        var data = Manage.fetchRequest(requestId);
+        if (data != null)
+        {
+            String range = "request!" + "A" + data[0] + ":C";
+            database.deleteData(range);
+        }
     }
 
 }
